298df1493760530dfcf7ce5451a091d1180a08b8
mertkalecik <mert.kalecik@hotmail.com>
2022-02-05 22:36:18 +0300

    Splash Screen Anim fix

--- app/src/main/java/com/mertkalecik/earthquake/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/mertkalecik/earthquake/ui/splash/SplashScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bolt
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -18,8 +19,6 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import com.mertkalecik.earthquake.navigation.Screen
 import com.mertkalecik.earthquake.ui.theme.Purple700
 import kotlinx.coroutines.delay
 
@@ -32,13 +31,13 @@ fun SplashScreen(
     val alphaAnim = animateFloatAsState(
         targetValue = if (startAnimation) 1F else 0F,
         animationSpec = tween(
-            durationMillis = 1000
+            durationMillis = 2000
         )
     )
 
     LaunchedEffect(key1 = true) {
         startAnimation = true
-        delay(2000)
+        delay(3000)
         onReady.invoke()
     }
     Splash(alpha = alphaAnim.value)
@@ -54,9 +53,9 @@ fun Splash(alpha: Float) {
     ) {
         Icon(
             modifier = Modifier
-                .size(120.dp)
+                .size(200.dp)
                 .alpha(alpha),
-            imageVector = Icons.Default.Email,
+            imageVector = Icons.Default.Bolt,
             contentDescription = "Logo Icon",
             tint = Color.White
         )
